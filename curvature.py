8378d4ae52cd8334a9fe065a4838f28a5fe0f6ed
crawlic-stud <nikitosik0726@gmail.com>
2021-11-02 20:14:21 +0300

    proper center box

--- curvature.py
+++ curvature.py
@@ -27,7 +27,7 @@ def generate_art(image, points_num, start=None, end=None, color=random_pastel,
         if curve_points:
             curve.draw_points(image, centered=True)
         else:
-            curve.draw(image, centered=True)
+            curve.draw(image, centered=False)
 
         image = Image.alpha_composite(image, curve.image)
 
@@ -37,10 +37,7 @@ def generate_art(image, points_num, start=None, end=None, color=random_pastel,
     polygon_image = Image.new('RGBA', image.size, (0, 0, 0, 0))
 
     polygon_pos = [tuple(start), (end[0], start[1]), tuple(end), (start[0], end[1])]
-    off_x, off_y = calculate_offset(polygon_image, polygon_pos)
-    polygon_pos = [(polygon_pos[i][0] + off_x, polygon_pos[i][1] - off_y) for i in range(len(polygon_pos))]
-
-    polygon_color = add_transparency(color(), random.randint(10, 50) / 100)
+    polygon_color = add_transparency(color(), random.randint(10, 30) / 100)
 
     ImageDraw.Draw(polygon_image).polygon(polygon_pos, fill=polygon_color)
 
@@ -54,18 +51,24 @@ if __name__ == '__main__':
     factor = size // 1000
     img = Image.new('RGBA', (size, size), (0, 0, 0, 0))
 
-    indent = 10
-    start = [random.randint(indent, size - indent), random.randint(indent, size - indent)]
-    end = [random.randint(indent, size - indent), random.randint(indent, size - indent)]
+    indent = 1000
+    # start = [random.randint(indent, size - indent), random.randint(indent, size - indent)]
+    # end = [random.randint(indent, size - indent), random.randint(indent, size - indent)]
+
+    start = [indent, indent]
+    end = [size - indent, size - indent]
+
+    box = [tuple(start), (end[0], start[1]), tuple(end), (start[0], end[1])]
+    off_x, off_y = calculate_offset(img, box)
 
     img = generate_art(image=img,
-                       points_num=5,
-                       start=start,
-                       end=end,
-                       color=random_blue,
+                       points_num=25,
+                       start=(start[0] + off_x, start[1] - off_y),
+                       end=(end[0] + off_x, end[1] - off_y),
+                       color=random_red,
                        color_change=True,
                        curve_points=False,
-                       steps=30,
+                       steps=20,
                        width=lambda: random.randint(1, 15),
                        draw_rect=True)
 
